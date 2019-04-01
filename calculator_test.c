8f95062bc61ff2238cc0622bf81b346cc139826e
Maria Volpe <mariaCHVolpe@gmail.com>
2019-04-01 02:10:55 -0400

    Call function from c math library

--- calculator_test.c
+++ calculator_test.c
@@ -1,12 +1,15 @@
 // test file to run c functions compiled via gcc
 #include <stdio.h>
+#include <math.h>
 
 int add(int a, int b) {
     return a + b;
 }
 
 int main(void) {
-    printf("%d", add(1, 2));
+    printf("%i \n", add(1, 2));
+    printf("%i \n", add(5, 3));
+    printf("%f \n", ldexp(11.0, 2));
 
     return 0;
 }
--- interface.ml
+++ interface.ml
@@ -3,7 +3,9 @@ open PosixTypes
 open Foreign
 
 let add = foreign "add" (int @-> int @-> returning int) ;;
+let ldexp = foreign "ldexp" (double @-> int @-> returning double) ;;
 
 let () =
     Printf.printf "%i \n" (add 1 1);
-    Printf.printf "%i" (add 5 3)
+    Printf.printf "%i \n" (add 5 3);
+    Printf.printf "%f" (ldexp 11.0 2)

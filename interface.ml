be91780f7e83dd965d41cf7b029b913a243f035e
Maria Volpe <mariaCHVolpe@gmail.com>
2019-04-28 00:48:17 -0400

    Build cJSON from shallow OCaml JSON

--- interface.ml
+++ interface.ml
@@ -8,7 +8,7 @@ let read_file = foreign "read_file" (string @-> returning string) ;;
 type name = string
 type value = Float of float
             | String of string
-            | Bool of bool
+            | Bool of int (* in cJSON, a bool is represented by an int *)
             | Child of json
             | Null
 and node = name * value
@@ -33,32 +33,18 @@ let valueint = field cJSON "valueint" int
 let valuedouble = field cJSON "valuedouble" double
 let name = field cJSON "string" string
 
-(* shorthand to allocate pointers for a given type *)
-let to_str_ptr str = allocate string str;;
-let to_int_ptr i = allocate int i;;
-let to_double_ptr dbl = allocate double dbl;;
-
-let sample_json = [ ("first_field", String "hello_world_1");
-                    ("second_field", String "hello_world_2");
-                    ("second_field", String "hello_world_2");
-                    ("third_field", Float 10.1)
-                    ]
-
-let rec print_ocaml_json ls =
-    match ls with
-    | (a, b) :: tl -> print_string a; print_ocaml_json tl
-    | [] -> ()
-
-let rec build_json ls =
-    match ls with
-    | (a, b) :: tl -> 
-    | [] -> 
-
-
 (* JSON FUNCTIONS *)
 
 (* CJSON_PUBLIC(cJSON * ) cJSON_CreateNumber(double num); *)
 let cJSON_CreateNumber = foreign "cJSON_CreateNumber" (double @-> returning (ptr cJSON)) ;;
+
+(* CJSON_PUBLIC(cJSON * ) cJSON_CreateString(const char *string); *)
+let cJSON_CreateString = foreign "cJSON_CreateString" (string @-> returning (ptr cJSON)) ;;
+
+(* CJSON_PUBLIC(cJSON * ) cJSON_CreateBool(cJSON_bool boolean); *)
+let cJSON_CreateBool = foreign "cJSON_CreateBool" (cJSON_bool @-> returning (ptr cJSON)) ;;
+
+
 
 (* CJSON_PUBLIC(cJSON * ) cJSON_CreateObject(void); *)
 let cJSON_CreateObject = foreign "cJSON_CreateObject" (void @-> returning (ptr cJSON)) ;;
@@ -75,17 +61,85 @@ let cJSON_Parse = foreign "cJSON_Parse" (string @-> returning (ptr cJSON)) ;;
 (* CJSON_PUBLIC(cJSON_bool) cJSON_IsTrue(const cJSON * const item); *)
 let cJSON_IsTrue = foreign "cJSON_IsTrue" (ptr cJSON @-> returning cJSON_bool) ;;
 
+
+
+(* shorthand to allocate pointers for a given type *)
+let to_str_ptr str = allocate string str;;
+let to_int_ptr i = allocate int i;;
+let to_double_ptr dbl = allocate double dbl;;
+
+let sample_json = [ ("first_field", String "hello_world_1");
+                    ("second_field", String "hello_world_2");
+                    ("second_field", String "hello_world_2");
+                    ("third_field", Float 10.1)
+                    ]
+
+(* let getValueTuple current =
+    let c_name = getf current name in
+    let c_type = begin getf current json_type end in
+    print_int c_type; 
+    []
+    (* match c_type with
+    | 0 -> ("c_name", Bool false)
+    | 1 -> ("c_name", Bool true)
+    | 2 -> ("c_name", Null)
+    | 3 -> ("c_name", Float begin getf current valuedouble end)
+    | 4 -> ("c_name", String begin getf current valuestring end) *)
+    (* | 5 -> (c_name, Child "placeholder") *)
+    (* | 6 -> (c_name, Child begin getf current next end) *)
+    (* | 7 -> (c_name, Child "placeholder")
+    | _ -> () TODO: raise exception, invalid json *) *)
+(* 
+let match_print item =
+    match item with
+    | Float f -> print_float f
+    | String s -> print_string s
+    | Bool b -> print_string b
+    | Child c -> 
+    | Null ->  *)
+
+
+let match_return item =
+    match item with
+    | Float f -> cJSON_CreateNumber f
+    | String s -> cJSON_CreateString s
+    | Bool b -> cJSON_CreateBool b
+    (* | Child c -> 
+    | Null ->  *)
+
+(* 
+let rec print_ocaml_json ls =
+    match ls with
+    | (a, b) :: tl -> print_string a; print_ocaml_json tl
+    | [] -> () *)
+
+let build_json ls = 
+    let base_cJSON = cJSON_CreateObject () in
+    let rec build ls =
+        match ls with
+        | (a, b) :: tl -> cJSON_AddItemToObject base_cJSON a begin match_return b end; build tl
+        | [] -> ()
+    in let _ = build ls in base_cJSON
+
+
+
 (* small, temporary run test *)
 let () =
     Printf.printf "%s \n" (read_file "json/shallow.json")
-    let str = read_file "json/shallow.json"
+    (* let str = read_file "json/shallow.json"
     let ptr_cJSON = cJSON_Parse str
     let base_cJSON = cJSON_CreateObject ()
     let num_item = cJSON_CreateNumber 10.0
     let final = cJSON_AddItemToObject base_cJSON "my_num" num_item
     let str_2 = cJSON_Print base_cJSON
     let _ = print_string str_2
-    let _ = print_ocaml_json sample_json
+    let _ = print_ocaml_json sample_json *)
+    let json_results = build_json sample_json
+    let str = cJSON_Print json_results
+    let _ = print_string str
+    
+
+    
     
 
     

289c1b04f2274bb3a5c5e9202c5d7c2da3066f6a
Maria Volpe <mariaCHVolpe@gmail.com>
2019-04-28 10:23:50 -0400

    Cleanup with demonstration of fail

--- interface.ml
+++ interface.ml
@@ -33,47 +33,28 @@ let valueint = field cJSON "valueint" int
 let valuedouble = field cJSON "valuedouble" double
 let name = field cJSON "string" string
 
-(* UTILS GO HERE -- WRAPPER FUNCTIONS TO ACTUALLY BE ABLE TO PARSE OUT RESULTING TYPES *)
-
-
-(* let rec convert current =
-            (* if current = nill return  *)
-            (* then pull out name *)
-            (* then pull out value -- using match *)
-            then call with next *)
-(* let rec convert current json_hd json_ls =
-    if current = Nil
-        then return json_hd
-    else
-        current.
-
-
-let cJSONtoJSON cJSON_blurb =
-    let json = []
-    convert cJSON_blurb json json
-         *)
 
 let print_json ls =
     []
 
+(* failing b/c of getf *)
 let get field =
     (* getf cJSON_blurb field *)
     false
 
+(* TODO wrap C *)
 let ifNull arg =
     false
 
 let getValueTuple current =
     let c_name = getf current name in
     let c_type = begin getf current json_type end in
-    print_int c_type; 
-    []
-    (* match c_type with
+    match c_type with
     | 0 -> ("c_name", Bool false)
     | 1 -> ("c_name", Bool true)
     | 2 -> ("c_name", Null)
     | 3 -> ("c_name", Float begin getf current valuedouble end)
-    | 4 -> ("c_name", String begin getf current valuestring end) *)
+    | 4 -> ("c_name", String begin getf current valuestring end)
     (* | 5 -> (c_name, Child "placeholder") *)
     (* | 6 -> (c_name, Child begin getf current next end) *)
     (* | 7 -> (c_name, Child "placeholder")
@@ -87,24 +68,14 @@ let cJSONtoJSON cJSON_blurb =
             [begin getValueTuple current end]
             (* @ begin convert begin getf current next end end *)
     in convert cJSON_blurb
-(* 
-let run_test_files () =
+
+(* let run_test_files () =
     let files = [ "json/shallow.json"; "json/children.json"; "json/deep-children.json"; "json/array.json" ] in
     let rec run ls =
         match ls with
         | hd :: tl -> (cJSONtoJSON hd) :: run tl
         | [] -> []
     in let ls = run files in print_json ls *)
-
-(* shallow implementation *)
-(* let rec print_node node =
-    []
-    (* match node  *)
-
-let rec print_json ls =
-    match ls with
-    | hd :: tl -> print_node hd; print_json tl; ()
-    | [] -> () *)
 
 (* shorthand to allocate pointers for a given type *)
 let to_str_ptr str = allocate string str;;
@@ -124,12 +95,9 @@ let () =
     Printf.printf "%s \n" (read_file "json/shallow.json")
     let str = read_file "json/shallow.json"
     let ptr_cJSON = cJSON_Parse str
-    (* let coerce_ptr = coerce (ptr cJSON) cJSON *)
-    (* let new_cJSON = coerce ptr_cJSON) *)
-    (* let any = cJSONtoJSON new_cJSON *)
-    let any = getf ptr_cJSON next
-    (* let any = 
-        match new_cJSON with
+    let new_cJSON = !@ptr_cJSON
+    (* let _ = 
+        match ptr_cJSON with
         | cJSON -> print_string "cJSON"
         | _ -> print_string "not"
      *)

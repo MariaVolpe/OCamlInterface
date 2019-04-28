4e64802894ae2f8d2f693175cf9eb8101b8c891c
Maria Volpe <mariaCHVolpe@gmail.com>
2019-04-27 21:29:59 -0400

    Begin function to convert cJSON to OCaml variant type--define function to get value based on type

--- interface.ml
+++ interface.ml
@@ -6,10 +6,11 @@ let read_file = foreign "read_file" (string @-> returning string) ;;
 
 (* ocaml variant equivalent to cJSON c struct *)
 type name = string
-type value = Int of int
+type value = Float of float
             | String of string
             | Bool of bool
             | Child of json
+            | Null
 and node = name * value
 and json = node list
 
@@ -34,6 +35,66 @@ let name = field cJSON "string" string
 
 (* UTILS GO HERE -- WRAPPER FUNCTIONS TO ACTUALLY BE ABLE TO PARSE OUT RESULTING TYPES *)
 
+
+(* let rec convert current =
+            (* if current = nill return  *)
+            (* then pull out name *)
+            (* then pull out value -- using match *)
+            then call with next *)
+(* let rec convert current json_hd json_ls =
+    if current = Nil
+        then return json_hd
+    else
+        current.
+
+
+let cJSONtoJSON cJSON_blurb =
+    let json = []
+    convert cJSON_blurb json json
+         *)
+
+let get field =
+    (* getf cJSON_blurb field *)
+    false
+
+let ifNull arg =
+    false
+
+let getValueTuple current =
+    let c_name = getf current name in
+    let c_type = getf current json_type in
+    match c_type with
+    | 0 -> (c_name, Bool false)
+    | 1 -> (c_name, Bool true)
+    | 2 -> (c_name, Null)
+    | 3 -> (c_name, Float begin getf current valuedouble end)
+    | 4 -> (c_name, String begin getf current valuestring end)
+    (* | 5 -> (c_name, Child "placeholder") *)
+    (* | 6 -> (c_name, Child begin getf current next end) *)
+    (* | 7 -> (c_name, Child "placeholder")
+    | _ -> () TODO: raise exception, invalid json *)
+
+let cJSONtoJSON cJSON_blurb =
+    let json = [] in
+    let convert current ls =
+        if ifNull current
+            then []
+        else
+            []
+            (* let c_name = getf cJSON_blurb name in
+            let c_type = getf cJSON_blurb json_type in
+            match c_type with
+            | 0 -> (c_name, Bool false)
+            | 1 -> (c_name, Bool true)
+            | 2 -> (c_name, Null)
+            | 3 -> (c_name, Float 10.0)
+            | 4 -> (c_name, String "placeholder")
+            | 5 -> (c_name, Child "placeholder")
+            | 6 -> (c_name, Child "placeholder")
+            | 7 -> (c_name, Child "placeholder")
+            | _ -> () TODO: raise exception, invalid json *)
+    in ()
+
 let cJSONtoJSON hd =
     []
 
@@ -44,6 +105,16 @@ let run_test_files () =
         | hd :: tl -> (cJSONtoJSON hd) :: run tl
         | [] -> []
     in let _ = run files in ()
+
+(* shallow implementation *)
+let rec print_node node =
+    []
+    (* match node  *)
+
+let rec print_json ls =
+    match ls with
+    | hd :: tl -> print_node hd; print_json tl; ()
+    | [] -> ()
 
 (* shorthand to allocate pointers for a given type *)
 let to_str_ptr str = allocate string str;;
